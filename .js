4b0031f9d1f3aa3de098dd77f7ead868e2b787c3
Kairi Karol <153057279+kairikarol30@users.noreply.github.com>
2024-11-07 00:58:24 -0600

    Primer commit con los archivos de la página web

---
+++ .js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var title = document.getElementById("main-title");
+    setTimeout(function() {
+        title.classList.add("fade-out");
+    }, 6000); 
+});
